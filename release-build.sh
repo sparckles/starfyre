2866b3fd2efb1f7c4e5600b8d76169f8e203aa47
Sanskar Jethi <sansyrox@gmail.com>
2023-02-22 22:45:08 +0000

    Release v0.5.0

--- release-build.sh
+++ release-build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-RUSTUP_TOOLCHAIN=nightly maturin build --target wasm32-unknown-emscripten -i python3.10 
+RUSTUP_TOOLCHAIN=nightly maturin build --target wasm32-unknown-emscripten -i python3.10 --out dist --sdist  --universal2 --strip
