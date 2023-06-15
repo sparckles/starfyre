d99cf35ee838c7c3a226e013c4cfe0063b86fedb
Damir <dtemi2@uis.edu>
2023-06-14 21:56:08 -0500

    Fix build.sh to run relative to the root of directory

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # RUSTUP_TOOLCHAIN=nightly maturin build --target wasm32-unknown-emscripten -i python3.10 --out dist
 # cp dist/starfyre*.whl test-application/starfyre-dist
 
-python3 starfyre --dev=True --path="/Users/sanskar/repos/starfyre/test-application/" && python -m starfyre --build=True --path="/Users/sanskar/repos/starfyre/test-application/"
+python3 starfyre --dev=True --path="test-application/" && python -m starfyre --build=True --path="test-application/"
