a405bc05547b26b21d6144f7e15c7e5d738d36b5
Akindele Michael <49593618+DeleMike@users.noreply.github.com>
2023-08-25 19:46:16 +0100

    feat: file based routing for starfyre (#67)
    * generate routes
    
    from the app_directory/pages, we generate in the generated dist folder, another folder called 'pages' where we all our generated routes will stored
    
    * feat(compiler.py-router.py): generate python build code for user defined routes
    
    * send output of render into generated html file
    
    * fix(router): Enhance route generation
    
    * fix generator bug
    
    * refactor file gen logic from router to main.py
    
    * rename functions and add docstrings
    
    * add more docstring to file router
    
    * build render output for component
    
    * create template file for build/__main__.py construction
    
    * fix app imports and improve code style
    
    * use descriptive names

---
+++ starfyre/build_main_file_content.py
@@ -0,0 +1,76 @@
+def get_build_main_file_content(user_routes, path):
+    """
+    Template for the main execution script to generate HTML pages for generated routes.
+
+    This module defines the template for the main execution script that generates HTML pages
+    for each route in the provided list of generated routes. The script imports the necessary
+    components for each route, renders them using Starfyre, and writes the rendered content
+    to corresponding HTML files.
+
+    The template contains placeholders for variables that will be dynamically substituted when
+    the script is used. The placeholders include the generated routes list and the path to the
+    project directory.
+    """
+
+    return f"""import os
+import sys
+from pathlib import Path
+import importlib
+
+
+def generate_pages(generated_routes, path):
+    '''
+    Generate HTML pages for each route in the provided list of routes.
+
+    Parameters:
+    - generated_routes (list): List of generated routes.
+    - path (str): Path to the project directory.
+
+    This function generates HTML pages for each route provided in the `generated_routes` list.
+    It imports the necessary components for each route, renders them using Starfyre,
+    and writes the rendered content to corresponding HTML files.
+    '''
+
+    out_dir = Path(path + "/dist").resolve()
+    root = Path(out_dir / "..").resolve()
+
+    # get the user defined project name
+    app_name = (str(root).split('/'))[-1]
+
+    for route_name in generated_routes:
+        print(f'route name is = {{route_name}}')
+        
+        if route_name.lower() == 'app':
+            component_key = 'app'  # For the 'app' component in `build/pages/__init__.py`
+        else:
+            component_key = route_name
+        
+        if route_name == 'app':
+            module_name = f"{{Path(app_name)}}.build.pages"
+        else:
+            module_name= f"{{Path(app_name)}}.build.pages.{{route_name}}"
+        
+        try:
+            module = importlib.import_module(module_name)
+            if component_key == 'app':
+                component = getattr(module, component_key, f"{{component_key}} does not exist")
+            else:
+                component = getattr(module, f'rendered_{{component_key}}')
+            result = str(component)
+        except ModuleNotFoundError:
+            print(f"Error: Could not import module '{{module_name}}'.")
+            continue
+
+        # write to component file
+        if route_name == 'app':
+            route_name = 'index'  # rename to index
+        with open(out_dir / f"{{route_name}}.html", "w") as html_file:
+            html_file.write("<script src='store.js'></script>")
+            html_file.write(result)
+
+
+
+if __name__ == '__main__':
+    generate_pages(generated_routes={user_routes}, path="{path}")
+    """
+
---
+++ starfyre/file_router.py
@@ -0,0 +1,64 @@
+from starfyre import create_component, render_root
+from starfyre.exceptions import IndexFileConflictError
+
+import os
+import sys
+from pathlib import Path
+import importlib
+
+
+class FileRouter:
+    def __init__(self, pages_directory):
+        """
+        A router that handles file-based routing.
+
+        This router parses the specified pages directory to automatically generate routes based on
+        the file names. Each file in the pages directory is treated as a separate route. 
+
+        Parameters:
+            pages_directory (str): The path to the directory containing the pages files.
+        Example:
+            pages_directory = "test-application/pages"
+            file_router = FileRouter(pages_directory)
+            Initialize the FileRouter with the specified pages directory.
+
+        Args:
+            pages_directory (str): The path to the directory containing the pages files.
+        """
+        self.pages_directory = pages_directory
+
+    def populate_router(self):
+        """
+        Collect route names from files in the specified pages directory.
+
+        This method collects route names based on the file names in the specified pages directory.
+        Each file in the pages directory with a ".fyre" extension is considered a separate route.
+        The route names are derived from the file names by removing the ".fyre" extension and
+        converting the names to lowercase.
+
+        The generated route names are stored in a list, and corresponding HTML files are created
+        in the specified "dist" directory.
+
+        Returns:
+            list: A list of generated route names.
+
+        Raises:
+            FileNotFoundError: If the specified pages directory does not exist.
+        """
+        routes = []
+        dist_dir = Path(self.pages_directory / ".." / "dist").resolve()
+        if not dist_dir.exists():
+            dist_dir.mkdir()
+
+        # get file names in the "pages" directory
+        for file_name in os.listdir(self.pages_directory):
+            if file_name.endswith(".fyre"):
+                route_name = file_name.replace(".fyre", "").lower()
+                if route_name == '__init__':
+                    routes.insert(0, 'app')
+                    continue # do no add '__init__' as a route found rather use 'app'
+                if route_name.lower() == 'index':
+                    raise IndexFileConflictError()
+                routes.append(route_name)
+
+        return routes
