8e244a0eb1ba9c5deaeda398e93a1c1037fef6cd
Sanskar Jethi <sansyrox@gmail.com>
2023-09-02 03:43:35 +0200

    add parser explanation

--- starfyre/parser.py
+++ starfyre/parser.py
@@ -18,17 +18,41 @@ def extract_functions(obj):
 
 
 class ComponentParser(HTMLParser):
-    # this is the grammar for the parser
-    # we need cover all the grammar rules
+    """
+    The ComponentParser is a custom HTML parser designed to parse custom components written in a specific format (resembling HTML). When parsing HTML-like structures, one of the most critical elements to handle is the hierarchy of tags: parent-child relationships.
 
-    def __init__(self, component_local_variables, component_global_variables, css, js, component_name):
-        super().__init__()             
-        self.stack: list[Component] = [] 
+    The self.stack attribute maintains a collection of components that are currently "open". When you encounter a start tag, you add to the stack. When you find an end tag, you pop off the stack. This allows you to maintain the nested structure inherent to HTML and similar languages.
+
+    The self.current_children list comes into play when dealing with custom components that can be nested inside other components. This list holds the currently parsed children for a custom component, which may not directly belong to the last item in the stack but instead belong to a custom component tag within that stack item.
+
+    Here's why self.current_children is necessary:
+
+    Slots and Custom Components: In the handle_endtag method, there's logic that looks for a slot component. This slot seems to be a placeholder where child components get injected. If there's a slot in the component being processed, then the current children (from self.current_children) get placed into that slot. If there's no slot, the children simply get appended to the component.
+
+    Handling custom components: If you encounter a tag that's not a generic HTML tag but is one of your custom components, the parser needs to know what child components belong to it. Instead of immediately adding children to the stack's top component, they are kept in self.current_children until it's clear where they should go.
+
+    End Tag Handling: When the end tag of a custom component is encountered, the parser checks if this custom component has any "reserved slots" for its children. If such slots are found, the children from self.current_children are placed inside those slots. Otherwise, the parser checks the parent of the current custom component (using the stack) and appends the children to it.
+
+    In summary, the self.current_children list is a temporary storage for child components while the parser is determining their final parent component. This is crucial when dealing with custom component logic, which may dictate where and how child components are placed within their parent component.
+
+
+    TODO: All of this can be achieved using a single stack(*batman's spider sense*), but I am not sure how to do it. I will try to do it later.
+    """
+
+    def __init__(
+        self,
+        component_local_variables,
+        component_global_variables,
+        css,
+        js,
+        component_name,
+    ):
+        super().__init__()
+        self.stack: list[Component] = []
         self.current_depth = 0
         self.css = css
         self.js = js
         self.root_node = None
-                
 
         # these are the event handlers and the props
         self.local_variables = component_local_variables
@@ -42,18 +66,108 @@ class ComponentParser(HTMLParser):
         self.current_children = []
 
     generic_tags = {
-        "html", "div", "p", "b", "span", "i", "button", "head", "link", "meta", "style", "title",
-        "body", "section", "nav", "main", "hgroup", "h1", "h2", "h3", "h4", "h5", "h6",
-        "header", "footer", "aside", "article", "address", "blockquote", "dd", "dl", "dt",
-        "figcaption", "figure", "hr", "li", "ol", "ul", "menu", "pre", "a", "abbr", "bdi",
-        "bdo", "br", "cite", "code", "data", "em", "mark", "q", "s", "small", "strong", "sub",
-        "sup", "time", "u", "area", "audio", "img", "map", "track", "video", "embed", "iframe",
-        "picture", "object", "portal", "svg", "math", "canvas", "script", "noscript", "caption",
-        "col", "colgroup", "table", "tbody", "td", "tfoot", "th", "thead", "tr", "datalist",
-        "fieldlist", "form", "input", "label", "legend", "meter", "optgroup", "option", "output",
-        "progress", "select", "textarea", "details", "dialog", "summary", "slot"
+        "html",
+        "div",
+        "p",
+        "b",
+        "span",
+        "i",
+        "button",
+        "head",
+        "link",
+        "meta",
+        "style",
+        "title",
+        "body",
+        "section",
+        "nav",
+        "main",
+        "hgroup",
+        "h1",
+        "h2",
+        "h3",
+        "h4",
+        "h5",
+        "h6",
+        "header",
+        "footer",
+        "aside",
+        "article",
+        "address",
+        "blockquote",
+        "dd",
+        "dl",
+        "dt",
+        "figcaption",
+        "figure",
+        "hr",
+        "li",
+        "ol",
+        "ul",
+        "menu",
+        "pre",
+        "a",
+        "abbr",
+        "bdi",
+        "bdo",
+        "br",
+        "cite",
+        "code",
+        "data",
+        "em",
+        "mark",
+        "q",
+        "s",
+        "small",
+        "strong",
+        "sub",
+        "sup",
+        "time",
+        "u",
+        "area",
+        "audio",
+        "img",
+        "map",
+        "track",
+        "video",
+        "embed",
+        "iframe",
+        "picture",
+        "object",
+        "portal",
+        "svg",
+        "math",
+        "canvas",
+        "script",
+        "noscript",
+        "caption",
+        "col",
+        "colgroup",
+        "table",
+        "tbody",
+        "td",
+        "tfoot",
+        "th",
+        "thead",
+        "tr",
+        "datalist",
+        "fieldlist",
+        "form",
+        "input",
+        "label",
+        "legend",
+        "meter",
+        "optgroup",
+        "option",
+        "output",
+        "progress",
+        "select",
+        "textarea",
+        "details",
+        "dialog",
+        "summary",
+        "slot",
     }
-              
 
     def extract_components(self, local_functions):
         components = {}
@@ -63,10 +177,9 @@ class ComponentParser(HTMLParser):
 
         return components
 
-
     def is_event_listener(self, name):
         return name.startswith("on")
-        
+
     def handle_starttag(self, tag, attrs):
         # logic should be to just create an empty component on start
         # and fill the contents on the end tag
@@ -104,7 +217,6 @@ class ComponentParser(HTMLParser):
             else:
                 props[attr[0]] = attr[1]
 
-
         # if the tag is not found in the generic tags but found in custom components
         if tag not in self.generic_tags and tag in self.components:
             component = self.components[tag]
@@ -112,7 +224,10 @@ class ComponentParser(HTMLParser):
             component.props = {**component.props, **props}
             component.state = {**component.state, **state}
 
-            component.event_listeners = {**component.event_listeners, **event_listeners}
+            component.event_listeners = {
+                **component.event_listeners,
+                **event_listeners,
+            }
             self.stack.append(component)
             print("This is the stack", self.stack)
             if self.root_node is None:
@@ -121,7 +236,9 @@ class ComponentParser(HTMLParser):
 
         # if the tag is not found in the generic tags and custom components
         if tag not in self.generic_tags and tag not in self.components:
-            raise UnknownTagError(f'Unknown tag: "{tag}". Please review line {self.lineno} in your "{self.component_name}" component in the pyml code.')
+            raise UnknownTagError(
+                f'Unknown tag: "{tag}". Please review line {self.lineno} in your "{self.component_name}" component in the pyml code.'
+            )
 
         if self.root_node is None:
             component = Component(
@@ -149,16 +266,17 @@ class ComponentParser(HTMLParser):
                 original_name=tag,
             )
 
-        # instead of assiging tags we assign uuids        
-        
+        # instead of assiging tags we assign uuids
+
         self.stack.append(component)
-        
 
     def handle_endtag(self, tag):
         # if the tag is not found in the generic tags and custom components
-        if tag not in self.generic_tags and tag not in self.components:             
-            raise UnknownTagError(f'Unknown tag: "{tag}". Please review line {self.lineno} in your "{self.component_name}" component in the pyml code.')
-        
+        if tag not in self.generic_tags and tag not in self.components:
+            raise UnknownTagError(
+                f'Unknown tag: "{tag}". Please review line {self.lineno} in your "{self.component_name}" component in the pyml code.'
+            )
+
         endtag_node = self.stack.pop()
         self.current_depth -= 1
 
@@ -168,18 +286,22 @@ class ComponentParser(HTMLParser):
             new_children = []
             is_slot_used = False
             for child_component in endtag_node.children:
-                if child_component.is_slot_component:   #We check each child in the entag_node list for the slot components 
+                if (
+                    child_component.is_slot_component
+                ):  # We check each child in the entag_node list for the slot components
                     new_children.extend(self.current_children)
                     is_slot_used = True
                 else:
                     new_children.append(child_component)
 
             if not is_slot_used and len(self.current_children) > 0:
-                #If there arent slot tag on the template.fyre and
-                # the current_children is not empty, means that the user forget 
+                # If there arent slot tag on the template.fyre and
+                # the current_children is not empty, means that the user forget
                 # to add the slot tag, so we add the content to the end and let the user knows
                 new_children.extend(self.current_children)
-                print("Appending at the end of the stack as slot position not specified.")
+                print(
+                    "Appending at the end of the stack as slot position not specified."
+                )
 
             if endtag_node.original_name != endtag_node.tag:
                 # if the tag is not found in the generic tags but found in custom components
@@ -188,9 +310,11 @@ class ComponentParser(HTMLParser):
                 # @suelen can you come up with a better explanation for this?
                 endtag_node.children = new_children
                 self.current_children = []
-                
+
             if len(self.stack) > 0:
-                parent_node = self.stack[-1]      #this is last item/"top element" of stack
+                parent_node = self.stack[
+                    -1
+                ]  # this is last item/"top element" of stack
                 if parent_node.original_name != parent_node.tag:
                     self.current_children.append(endtag_node)
                 else:
@@ -199,8 +323,6 @@ class ComponentParser(HTMLParser):
                 self.root_node = endtag_node
                 self.root_node.children.extend(self.current_children)
                 self.current_children = []
-
-   
 
     def is_signal(self, str):
         if not str:
@@ -224,8 +346,8 @@ class ComponentParser(HTMLParser):
 
         # parsing starts here
         state = {}
-        parent_node = self.stack[-1]    
-        
+        parent_node = self.stack[-1]
+
         uuid = uuid4()
         component_signal = ""
 
@@ -253,7 +375,9 @@ class ComponentParser(HTMLParser):
                     )
                     if isinstance(eval_result, Component):
                         # this is a parent, right?
-                        self.children.append(eval_result) #TODO: Check with sanskar - this is a bug, we don't have self.children
+                        self.children.append(
+                            eval_result
+                        )  # TODO: Check with sanskar - this is a bug, we don't have self.children
                         return
                     elif isinstance(eval_result, str):
                         current_data = eval_result
@@ -315,12 +439,11 @@ class ComponentParser(HTMLParser):
             )
         )
 
-        #We need to check if the wrapped component should go to the custom list aka self.current_children or to parent_node children list
+        # We need to check if the wrapped component should go to the custom list aka self.current_children or to parent_node children list
         if parent_node.original_name != parent_node.tag:
             self.current_children.append(wrapper_div_component)
         else:
             parent_node.children.append(wrapper_div_component)
-                      
 
     def get_stack(self):
         return self.stack
@@ -329,5 +452,14 @@ class ComponentParser(HTMLParser):
         if self.root_node is not None:
             return self.root_node
         return Component(
-            "div", {}, [], {}, state={}, data="", css=self.css, js=self.js, uuid=uuid4(), original_name="div"
+            "div",
+            {},
+            [],
+            {},
+            state={},
+            data="",
+            css=self.css,
+            js=self.js,
+            uuid=uuid4(),
+            original_name="div",
         )
